01f8bd0f531034846f9a6fbf065d7928b4b0e8fc
manpreetsuffescom <manpreet@suffescom.co>
2024-07-31 18:39:06 +0530

    onramp changes

--- apps/web/runbuild.js
+++ apps/web/runbuild.js
@@ -1,19 +1,12 @@
 const express = require("express");
 const path = require("path");
 const logger = require("morgan");
-const helmet = require('helmet');
+
 const app = express();
 
 app.use(logger("dev"));
 
-app.use(
-  helmet.contentSecurityPolicy({
-    directives: {
-      defaultSrc: ["'self'"],
-      frameAncestors: ["'self'", 'https://*.onramper.com', 'https://onramper.com', 'https://www.onramper.com', 'https://localhost', 'http://localhost',"https://goblins.cash/", "https://dex.goblins.cash/"]
-    }
-  })
-);
+
 
 app.use(express.static(path.join(__dirname, "build")));
 
