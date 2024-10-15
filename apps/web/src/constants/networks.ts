ee3f2aac6123b422554071c4c28592de6c7282d5
manpreet <manpreetsuffescom.co>
2024-10-15 10:52:56 +0530

    rpc changing

--- apps/web/src/constants/networks.ts
+++ apps/web/src/constants/networks.ts
@@ -12,7 +12,7 @@ const QUICKNODE_ARBITRUM_RPC_URL = process.env.REACT_APP_QUICKNODE_ARBITRUM_RPC_
 if (typeof QUICKNODE_ARBITRUM_RPC_URL === 'undefined') {
   throw new Error(`REACT_APP_QUICKNODE_ARBITRUM_RPC_URL must be a defined environment variable`)
 }
-const QUICKNODE_BNB_RPC_URL = "https://rough-sleek-hill.bsc.quiknode.pro/413cc98cbc776cda8fdf1d0f47003583ff73d9bf"
+const QUICKNODE_BNB_RPC_URL = "https://bsc-dataseed.binance.org"
 if (typeof QUICKNODE_BNB_RPC_URL === 'undefined') {
   throw new Error(`REACT_APP_BNB_RPC_URL must be a defined environment variable`)
 }
