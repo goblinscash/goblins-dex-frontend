5b8a4429451c632db70429fb0ecc43bad203551a
manpreet <manpreetsuffescom.co>
2024-10-15 18:23:05 +0530

    WDOGE token added

--- apps/web/src/constants/routing.ts
+++ apps/web/src/constants/routing.ts
@@ -52,7 +52,8 @@ import {
   WETH_POLYGON,
   WETH_POLYGON_MUMBAI,
   WRAPPED_NATIVE_CURRENCY,
-  BB_BTC
+  BB_BTC,
+  WDOGE_BCH
 } from './tokens'
 
 type ChainTokenList = {
@@ -143,7 +144,7 @@ export const COMMON_BASES: ChainCurrencyList = {
     USDT_AVALANCHE,
     WETH_AVALANCHE,
   ],
-  [ChainId.SMARTBCH]: [nativeOnChain(ChainId.SMARTBCH), WRAPPED_NATIVE_CURRENCY[ChainId.SMARTBCH] as Token, USDT_SBCH, BC_BCH, GOB, BB_USDC, BB_USDT, BB_ETH, BB_BCH, BB_BTC ],
+  [ChainId.SMARTBCH]: [nativeOnChain(ChainId.SMARTBCH), WRAPPED_NATIVE_CURRENCY[ChainId.SMARTBCH] as Token, USDT_SBCH, BC_BCH, GOB, BB_USDC, BB_USDT, BB_ETH, BB_BCH, BB_BTC, WDOGE_BCH ],
 }
 
 // used to construct the list of all pairs we consider by default in the frontend
--- apps/web/src/constants/tokens.ts
+++ apps/web/src/constants/tokens.ts
@@ -331,6 +331,14 @@ export const BB_BCH = new Token(
   'bbBCH'
 )
 
+export const WDOGE_BCH = new Token(
+  ChainId.SMARTBCH,
+  '0xdcDC535c134159AF7683df91CbAF014f88C5137d',
+  18,
+  'dcWDOGE',
+  'dcWDOGE'
+)
+
 export const WRAPPED_NATIVE_CURRENCY: { [chainId: number]: Token | undefined } = {
   ...(WETH9 as Record<ChainId, Token>),
   [ChainId.OPTIMISM]: new Token(
