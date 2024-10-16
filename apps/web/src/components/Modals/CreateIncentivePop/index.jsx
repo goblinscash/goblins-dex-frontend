7a035f9ba440a05cd509c8a9a3d000f945c91a1d
manpreet <manpreetsuffescom.co>
2024-10-16 13:10:49 +0530

    GC-271

--- apps/web/src/components/Modals/CreateIncentivePop/index.jsx
+++ apps/web/src/components/Modals/CreateIncentivePop/index.jsx
@@ -232,9 +232,9 @@ const CreateIncentivePop = ({ incentiveForm, setIncentiveForm, load }) => {
                         required
                       >
                         <option value="">Select Width</option>
+                        <option value={1}>1</option>
                         <option value={5}>5</option>
                         <option value={10}>10</option>
-                        <option value={15}>15</option>
                         <option value={20}>20</option>
                       </select>
                     </div>
