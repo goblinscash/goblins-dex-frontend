a9a4df4e1d64523fc4cca3c8976aaa92fe92260e
manpreet <manpreetsuffescom.co>
2024-11-21 16:55:21 +0530

    farming done

--- apps/web/src/helpers/constants.js
+++ apps/web/src/helpers/constants.js
@@ -106,6 +106,7 @@ export const graphQLUrl = {
 export const GOBAddress = {
   10000: "0x56381cb87c8990971f3e9d948939e1a95ea113a3",
   56: "0x701aca29ae0f5d24555f1e8a6cf007541291d110",
+  8453: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913"
  
 };
 
