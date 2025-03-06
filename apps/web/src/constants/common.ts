89f7926a5d8a9c885128c5674699efe4870f75ca
Ashutoshmaurya108 <ashutosh.kumar@suffescom.com>
2025-03-06 19:21:35 +0530

    update protocol fee reciever address

--- apps/web/src/constants/common.ts
+++ apps/web/src/constants/common.ts
@@ -1,5 +1,5 @@
 export const interFaceFeeAddress: { [key: number]: string } = {
-    "10000": "0x2deaEc93d899B9f7fA060b79078DEc170494ff24",
+    "10000": "0x2D3eec38707Da34a82cF81c874aE943E1Da9482b",
     "56": "0x2D3eec38707Da34a82cF81c874aE943E1Da9482b",
-    "8453": "0x51366a14Bb6969D1a9236776dEC5F368dA71Cde9"
+    "8453": "0x2D3eec38707Da34a82cF81c874aE943E1Da9482b"
 } 
