d54c4a1143b4292d1c1ee434dd977b23d6b68077
krishprism1 <ashutoshcodepro@gmail.com>
2025-06-04 08:11:39 +0530

    updated sbch rpc

--- apps/web/src/constants/networks.ts
+++ apps/web/src/constants/networks.ts
@@ -123,8 +123,8 @@ export const FALLBACK_URLS = {
     'https://1rpc.io/base',
     'https://base.meowrpc.com',
   ],
-  [ChainId.SMARTBCH]: ['https://smartbch.fountainhead.cash/mainnet', 'https://smartbch.greyh.at', 'https://global.uat.cash',
-    'https://smartbch.greyh.at',
+  [ChainId.SMARTBCH]: ['https://smartbch.fountainhead.cash/mainnet', 'https://smartbch.fountainhead.cash/mainnet', 'https://global.uat.cash',
+    'https://smartbch.fountainhead.cash/mainnet',
     'https://rpc.smartbch.org']
 }
 
@@ -164,5 +164,5 @@ export const RPC_URLS = {
   [ChainId.BNB]: [QUICKNODE_BNB_RPC_URL, ...FALLBACK_URLS[ChainId.BNB]],
   [ChainId.AVALANCHE]: [`https://avalanche-mainnet.infura.io/v3/${INFURA_KEY}`, ...FALLBACK_URLS[ChainId.AVALANCHE]],
   [ChainId.BASE]: ['https://mainnet.base.org',`https://base-mainnet.infura.io/v3/${INFURA_KEY}`, ...FALLBACK_URLS[ChainId.BASE]],
-  [ChainId.SMARTBCH]: [`https://smartbch.greyh.at`, ...FALLBACK_URLS[ChainId.SMARTBCH]],
+  [ChainId.SMARTBCH]: [`https://smartbch.fountainhead.cash/mainnet`, ...FALLBACK_URLS[ChainId.SMARTBCH]],
 }
