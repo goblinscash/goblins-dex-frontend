7f83fe0fa14ec77bfc6ec633dc4502051e365b27
devprotocol1 <dev.protocol1@gmail.com>
2025-06-05 22:48:31 +0530

    updated the timer

--- apps/web/src/components/NavBar/index.tsx
+++ apps/web/src/components/NavBar/index.tsx
@@ -157,7 +157,7 @@ const Navbar = ({ blur }: { blur: boolean }) => {
       {/* {blur && <Blur />} */}
       <TimeWrapper>
         <p style={{ margin: 0, fontSize: 12, textAlign: "center" }}>Aerodrome Fork on BSC – Coming Soon!</p>
-        <CountdownTimer targetDate='2025-06-01T23:59:59' />
+        <CountdownTimer targetDate='2025-07-01T23:59:59' />
       </TimeWrapper>
       <Nav>
         <Box display="flex" height="full" flexWrap="nowrap" >
